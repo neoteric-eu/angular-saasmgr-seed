e51678bc65a0f420031ff7befe216009334bf54e
Przemysław Czekaj <pczekaj@neoteric.eu>
2016-04-18 16:08:32 +0200

    feat(auth): auhtentication example

--- app/app.js
+++ app/app.js
@@ -3,10 +3,44 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
+  'myApp.auth',
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
+]).value('apiUrl', 'http://dev.sharedservices.ntrc.eu:4100/api/v2').config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
+}]).controller('AppCtrl', ['$scope', '$location', 'User', function ($scope, $location, User) {
+  $scope.appReady = false;
+
+  User.getInfo().then(function () {
+    $location.path('/view1');
+  }).catch(function () {
+    $location.path('/login');
+  }).finally(function () {
+    $scope.appReady = true;
+  });
+
+  $scope.isActive = function (location) {
+    return location === $location.path();
+  };
+
+  $scope.logout = function () {
+    User.logout().then(function () {
+      $scope.user = null;
+      $location.path('/login');
+    });
+  };
+
+  $scope.$on(User.update_broadcast, function () {
+    $scope.user = User.model;
+  });
+
+  $scope.$on('$routeChangeStart', function (event, next) {
+    if (next.$$route !== undefined) {
+      var nextRoute = next.$$route.originalPath;
+      if (User.model.id === undefined && (nextRoute != '/register' && nextRoute != '/login')) {
+        $location.path('/login');
+      }
+    }
+  });
 }]);
---
+++ app/auth/user.js
@@ -0,0 +1,83 @@
+'use strict';
+
+angular.module('myApp.auth')
+.service('User', ['$http', '$rootScope', 'apiUrl', function ($http, $rootScope, apiUrl) {
+    var user = {}, storageSettings = {
+        token: 'token',
+        customer: 'customerId'
+    };
+
+    user.model = {};
+
+    user.registration = function (user_info) {
+        user_info.language = 'en_GB';
+        user_info.requireConfirmation = false;
+        return $http.post(apiUrl + user.urls.register_user, user_info).then(function (response) {
+            response.data.data = {
+                token: response.data.data.token,
+                user: response.data.data
+            };
+            setSession(response);
+            return user.getInfo();
+        });
+    };
+
+    user.login = function (credentials) {
+        return $http.post(apiUrl + user.urls.login_user, credentials).then(function (response) {
+            setSession(response);
+            return user.getInfo();
+        });
+    };
+
+    user.getInfo = function () {
+        restoreSession();
+        return $http.get(apiUrl + user.urls.get_user_info).then(function (response) {
+            user.model = response.data.data.user;
+            $rootScope.$broadcast(user.update_broadcast);
+        }).catch(function () {
+            clearSession();
+        });
+    };
+
+    user.logout = function () {
+        return $http.post(apiUrl + user.urls.logout_user).finally(function () {
+            clearSession();
+        });
+    };
+
+    function restoreSession() {
+        var token = sessionStorage.getItem(storageSettings.token),
+            customer = sessionStorage.getItem(storageSettings.customer);
+
+        if(token) {
+            $http.defaults.headers.common.Authorization = 'Token ' + token;
+        }
+        if(customer) {
+            $http.defaults.headers.common['X-Customer-Id'] = customer;
+        }
+    }
+
+    function setSession(response) {
+        sessionStorage.setItem(storageSettings.token, response.data.data.token);
+        sessionStorage.setItem(storageSettings.customer, response.data.data.user.customers[0].customerId);
+    }
+
+    function clearSession() {
+        user.model = {};
+        sessionStorage.removeItem(storageSettings.token);
+        sessionStorage.removeItem(storageSettings.customer);
+        $http.defaults.headers.common.Authorization = '';
+        $http.defaults.headers.common['X-Customer-Id'] = '';
+    }
+
+    // User constants
+    user.update_broadcast = 'user-updated';
+    user.urls = {
+        login_user: '/users/login',
+        logout_user: '/users/logout',
+        get_user_info: '/users/authInfo',
+        register_user: '/registration'
+    };
+
+    return user;
+}]);
